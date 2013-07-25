63ab3a6ba5784b3eb5b6550f672eef8e2e4cba9e
javakat <javakatdesign@gmail.com>
2013-07-25 13:43:48 -0500

    Update data structure stuff.

--- queue.cpp
+++ queue.cpp
@@ -1,9 +1,26 @@
 namespace ego {
   class Queue {
-    
+    Node *head;
+
     public:
     Queue (string filename) {
       /* Open 'filename' and add the trace elements to the list. */ 
     }
+
+    void push(Node *n){
+      Node *current = head;
+      while(current) {
+        if (current->next == NULL) {
+          current->next = n;
+          return;
+        }
+        current = current->next;
+      }
+    }
+
+    void addToFront(Node *n) {
+      n->next = head;
+      head = n;
+    } 
   };
 }
