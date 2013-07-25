bdc3649d4a39239075e4472b84a3e32a770c3f5a
Zachary D White <zdw0001@tux201.eng.auburn.edu>
2013-07-25 17:42:58 -0500

    Update Scheduler.cpp.

--- scheduler.cpp
+++ scheduler.cpp
@@ -19,7 +19,19 @@ namespace ego {
     }
 
     void expsort(Queue *q) {
-    
+        long int timesliceusage;//get this somehow
+        //check for new job
+        Node * head = q -> getHead();
+        long int priority = head.getPriority();
+        q.pop();
+        long int timeSlice = (2^(priority - 1)) * 10;
+        if ((timesliceusage < (timeSlice / 2) && priority != 1)) {
+          priority--;
+        }
+        else if ((timesliceusage == timeSlice) && priority != 8) {
+          priority++;
+        }
+        q.push(head);
     }
     
     void stfcpsort(Queue *q) {
