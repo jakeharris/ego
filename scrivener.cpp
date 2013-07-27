7c2deef069e92aa34ebcb72c505ab32cac8a9bef
Clay Miller <clay@smockle.com>
2013-07-26 21:40:52 -0500

    Modify variable definition.

--- scrivener.cpp
+++ scrivener.cpp
@@ -9,9 +9,9 @@ namespace ego {
     long int runTime; //needs to reset before each queue uses it
     long int numJobs; //needs to reset before each queue uses it
     long int use; //time doing usefull stuff
-    long int act; //
-    long int throughput; //
-    long int util; //
+    long double act; //
+    long double throughput; //
+    long double util; //
     
     public:
     Scrivener() {
@@ -40,18 +40,18 @@ namespace ego {
     }
 
     long double calAct() {
-      long double act = runTime / numJobs;//get runtime from node
+      act = runTime / numJobs;//get runtime from node
       return act;
     }
 
     long double calThroughput() {//get info from node
-      long double throughput = (numJobs / (runTime/1000));//per second so divide time by 1k?
+      throughput = (numJobs / (runTime/1000));//per second so divide time by 1k?
       return throughput;
     }
 
     long int calUtil() {
       long double scale = 100.01;
-      long double util = (use / runTime) * scale; //display as percentage and round to nearest hundredth, figure out how to get use time. possibly count everytime it's pushed?
+      util = (use / runTime) * scale; //display as percentage and round to nearest hundredth, figure out how to get use time. possibly count everytime it's pushed?
       return util;
     }
 
